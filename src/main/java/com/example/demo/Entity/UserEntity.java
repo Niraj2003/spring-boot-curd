a111986aeb229e3fc92d6535e9e6ccecb61da23a
Niraj Amrutkar <>
2024-02-07 01:41:17 +0530

    Update 1.1

--- src/main/java/com/example/demo/Entity/UserEntity.java
+++ src/main/java/com/example/demo/Entity/UserEntity.java
@@ -1,6 +1,5 @@
 package com.example.demo.Entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.Repository;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
